20fa53c0b759d1a6373863f87c6f6cfb5139fab0
Simon-Duc <91253888+Simon-Duc@users.noreply.github.com>
2022-03-07 15:05:04 +0100

    started the exercise by creating the files and added a few lines

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+// This is a comment
+
+/* This comment
+is on multiple lines */
+
+/**
+* This comment
+* is used for
+* documentation
+*/
+
+console.log(2*3);
+console.log(2*3);
